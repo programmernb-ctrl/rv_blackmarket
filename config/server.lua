36f1e1ee8d61f11d18b7553faa3fee6efa72a4d8
programmernb-ctrl <141086417+programmernb-ctrl@users.noreply.github.com>
2025-10-24 00:49:27 +0200

    feat: added configuration values
    * blackmoney item name
    * differentiate between buy/sell items

--- config/server.lua
+++ config/server.lua
@@ -1,10 +1,20 @@
 return {
 
+    -- ox_inventory blackmoney item name
+    blackmoney = 'black_money',
+
     -- ADD AS MUCH ITEMS YOU LIKE, OR CHANGE PRICES
     DealerItems = {
-        lockpick = math.random(100, 150),
-        armour = math.random(600, 900),
-        weapon_crowbar = math.random(200, 300)
+
+        buyable = {
+            lockpick = math.random(100, 150),
+            armour = math.random(600, 900),
+            weapon_crowbar = math.random(200, 300)
+        },
+
+        sellable = {
+            --
+        }
     }
 
 }
